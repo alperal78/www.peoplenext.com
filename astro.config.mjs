6368980c2296044ed4a5712c11b7305a4b873003
Alfredo Perez <alperal@gmail.com>
2026-08-20 22:26:32 -0500

    Ensure 100% canonical URLs include https://www.peoplenext.com domain and trailing slash

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://www.peoplenext.com',
   server: {
     host: '0.0.0.0',
     port: 4321
